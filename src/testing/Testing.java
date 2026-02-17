e709073e3f283644f3bb2f433d142881bcca1a7c
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-17 17:02:41 -0400

    Añade operacion de resta

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -13,8 +13,9 @@ public class Testing {
         int numero1,numero2,operacion,resultado;
         
         operacion = Integer.parseInt(JOptionPane.showInputDialog("Elija una operacion: \n"
-                + "1. Sumar: \n"
-                + "2. Salir: "));
+                + "1. Sumar \n"
+                + "2. Restar \n"
+                + "3. Salir: "));
         
         switch (operacion) {
             case 1: System.out.print("Digite la primera canticad: ");
@@ -26,8 +27,17 @@ public class Testing {
                     
                     System.out.println("El resultado es: "+resultado);
                     break;
+            case 2:  System.out.print("Digite la primera canticad: ");
+                    numero1 = entrada.nextInt();
+                    System.out.print("Digite la segunda cantidad: ");
+                    numero2 = entrada.nextInt();
                     
-            case: 2: break;
+                    resultado = numero1 - numero2;
+                    
+                    System.out.println("El resultado es: "+resultado);
+                    break;
+                
+            case: 3: break;
             
             default : System.out.println("Error, se equivoco de operacion");
     }
