dda6074b38afeb7ce1e454e791acd277860c3f57
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-17 15:43:17 -0400

    Se crea el scanner

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -1,12 +1,14 @@
 
 package testing;
 
+import java.util.Scanner;
+
 
 public class Testing {
 
     
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner (System.in);
         
         
     }
