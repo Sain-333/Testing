cc39f79fa8af07bafc8b28e0be80cd102145bd92
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-17 17:51:59 -0400

    Se añade la operacion dividir mas el .gitignore

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -16,7 +16,8 @@ public class Testing {
                 + "1. Sumar \n"
                 + "2. Restar \n"
                 + "3. Multiplicar\n"
-                + "4. Salir: "));
+                + "4. Dividir\n"
+                + "5. Salir: "));
         
         switch (operacion) {
             case 1: System.out.print("Digite la primera canticad: ");
@@ -48,7 +49,18 @@ public class Testing {
                     
                     System.out.println("El resultado es: "+resultado);
                     break;
-            case: 4: break;
+                    
+            case 4: System.out.print("Digite la primera canticad: ");
+                    numero1 = entrada.nextInt();
+                    System.out.print("Digite la segunda cantidad: ");
+                    numero2 = entrada.nextInt();
+                    
+                    resultado = numero1 / numero2;
+                    
+                    System.out.println("El resultado es: "+resultado);
+                    break;
+                
+            case 5: break;
             
             default : System.out.println("Error, se equivoco de operacion");
     }
