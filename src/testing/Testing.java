14f6354519d1340d84d5327158f708676ca5f272
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-17 19:48:11 -0400

    Se añade la operacion de potencia

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -11,13 +11,15 @@ public class Testing {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
         int numero1,numero2,operacion,resultado;
+        double base,exponente,resultado2;
         
         operacion = Integer.parseInt(JOptionPane.showInputDialog("Elija una operacion: \n"
                 + "1. Sumar \n"
                 + "2. Restar \n"
                 + "3. Multiplicar\n"
                 + "4. Dividir\n"
-                + "5. Salir: "));
+                + "5. Potencia\n"
+                + "6. Salir: "));
         
         switch (operacion) {
             case 1: System.out.print("Digite la primera canticad: ");
@@ -60,7 +62,16 @@ public class Testing {
                     System.out.println("El resultado es: "+resultado);
                     break;
                 
-            case 5: break;
+            case 5:  System.out.print("Digite la base: ");
+                     base = entrada.nextDouble();
+                     System.out.print("Digite el exponente: ");
+                     exponente = entrada.nextDouble();
+                     
+                     resultado2 = Math.pow(base, exponente);
+                    System.out.println("El resultado es : "+resultado2);
+                    break;
+                    
+            case 6: break;
             
             default : System.out.println("Error, se equivoco de operacion");
     }
